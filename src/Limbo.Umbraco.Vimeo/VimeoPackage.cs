5cf43bcfcd4c18fb54a898934a21a2143dcc474c
Anders Bjerner <abjerner@skybrud.dk>
2022-11-30 22:40:34 +0100

    Updated the package information

---
+++ src/Limbo.Umbraco.Vimeo/VimeoPackage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Skybrud.Essentials.Reflection;
+using Umbraco.Cms.Core.Semver;
+
+namespace Limbo.Umbraco.Vimeo {
+
+    /// <summary>
+    /// Static class with various information and constants about the package.
+    /// </summary>
+    public class VimeoPackage {
+
+        /// <summary>
+        /// Gets the alias of the package.
+        /// </summary>
+        public const string Alias = "Limbo.Umbraco.Vimeo";
+
+        /// <summary>
+        /// Gets the friendly name of the package.
+        /// </summary>
+        public const string Name = "Limbo Vimeo";
+
+        /// <summary>
+        /// Gets the version of the package.
+        /// </summary>
+        public static readonly Version Version = typeof(VimeoPackage).Assembly.GetName().Version!;
+
+        /// <summary>
+        /// Gets the informational version of the package.
+        /// </summary>
+        public static readonly string InformationalVersion = ReflectionUtils.GetInformationalVersion(typeof(VimeoPackage));
+
+        /// <summary>
+        /// Gets the semantic version of the package.
+        /// </summary>
+        public static readonly SemVersion SemVersion = SemVersion.Parse(ReflectionUtils.GetInformationalVersion<VimeoPackage>());
+
+        /// <summary>
+        /// Gets the URL of the GitHub repository for this package.
+        /// </summary>
+        public const string GitHubUrl = "https://github.com/limbo-works/Limbo.Umbraco.Vimeo";
+
+        /// <summary>
+        /// Gets the URL of the issue tracker for this package.
+        /// </summary>
+        public const string IssuesUrl = "https://github.com/limbo-works/Limbo.Umbraco.Vimeo/issues";
+
+        /// <summary>
+        /// Gets the URL of the documentation for this package.
+        /// </summary>
+        public const string DocumentationUrl = "https://packages.limbo.works/limbo.umbraco.vimeo/v2/docs/";
+
+    }
+
+}
